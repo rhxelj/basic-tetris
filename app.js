425d13619cc2f14eb55a8c205fea7083a2575f74
Rogelio M Corsino <rcorsino@gmail.com>
2020-10-08 13:57:33 -0300

    Add draw function

--- app.js
+++ app.js
@@ -40,7 +40,30 @@ document.addEventListener("DOMContentLoaded", () => {
     [1, width + 1, width * 2 + 1, width * 3 + 1],
     [width, width + 1, width + 2, width + 3],
   ];
+  const theTetrominoes = [
+    lTetromino,
+    zTetromino,
+    tTetromino,
+    oTetromino,
+    iTetromino,
+  ];
 
-  // lTeromino[3].forEach((s) => (squares[s].style.background = "blue"));
-  lTeromino[3].forEach((s) => squares[s + 3].classList.add("ltrominoColor"));
+  let currentPosition = 4;
+  let currentRotation = 0;
+  let random = Math.floor(Math.random() * theTetrominoes.length);
+
+  let current = theTetrominoes[random][currentRotation];
+
+  function draw() {
+    current.forEach((index) =>
+      squares[index + currentPosition].classList.add("tetromino")
+    );
+  }
+  function undraw() {
+    current.forEach((index) =>
+      squares[index + currentPosition].classList.remove("tetromino")
+    );
+  }
+
+  draw();
 });
