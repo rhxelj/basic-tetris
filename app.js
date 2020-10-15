0c4db717fcc0b70a3d698dd053db6f2f1f313b1b
Rogelio M Corsino <rcorsino@gmail.com>
2020-10-14 22:45:37 -0300

    add a minigrid

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const ScoreDisplay = document.querySelector("#score");
   const StartBtn = document.querySelector("start-button");
   const width = 10;
-
+  var speed = 1000;
   //The Tetrominoes
   const lTetromino = [
     [1, width + 1, width * 2 + 1, 2],
@@ -68,14 +68,44 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // I put the tetromino to move
 
-  intervalID = window.setInterval(movedown, 1000);
+  // intervalID = window.setInterval(movedown, 1000);
+  intervalID = window.setInterval(movedown, speed);
+  console.log("intervalID --> ", intervalID);
 
+  function speedUp() {
+    window.clearInterval(intervalID);
+    speed -= 100;
+    intervalID = window.setInterval(movedown, speed);
+    console.log("velocidad = ", speed);
+  }
   function movedown() {
     undraw();
     currentPosition += width;
     draw();
     freeze();
   }
+  function rotate() {
+    undraw();
+    if (currentRotation === 3) {
+      currentRotation = 0;
+    } else {
+      currentRotation += 1;
+      current = theTetrominoes[random][currentRotation];
+    }
+  }
+  function control(e) {
+    if (e.keyCode === 37) {
+      moveLeft();
+    } else if (e.keyCode === 39) {
+      moveRight();
+    } else if (e.keyCode === 40) {
+      speedUp();
+    } else if (e.keyCode === 38) {
+      rotate();
+      console.log("currentRotation ", currentRotation);
+    }
+  }
+  document.addEventListener("keyup", control);
 
   function freeze() {
     if (
@@ -87,11 +117,44 @@ document.addEventListener("DOMContentLoaded", () => {
         squares[index + currentPosition].classList.add("taken")
       );
       currentPosition = 4;
-      currentRotation = 0;
       random = Math.floor(Math.random() * theTetrominoes.length);
 
       current = theTetrominoes[random][currentRotation];
+      speed = 1000;
+      currentRotation = 0;
       draw();
     }
+  }
+
+  function moveLeft() {
+    undraw();
+    const isAtLeftEdge = current.some(
+      (index) => (currentPosition + index) % width === 0
+    );
+    if (!isAtLeftEdge) currentPosition -= 1;
+    if (
+      current.some((index) =>
+        squares[currentPosition + index].classList.contains("taken")
+      )
+    ) {
+      currentPosition += 1;
+    }
+
+    draw();
+  }
+  function moveRight() {
+    undraw();
+    const isARightEdge = current.some(
+      (index) => (currentPosition + index) % width === width - 1
+    );
+    if (!isARightEdge) currentPosition += 1;
+    if (
+      current.some((index) =>
+        squares[currentPosition + index].classList.contains("taken")
+      )
+    ) {
+      currentPosition -= 1;
+    }
+    draw();
   }
 });
